287aa1e93874157d8b2897ddb433a7ae044e8729
Md Mynuddin <60794460+Mynuddin-dev@users.noreply.github.com>
2022-08-14 19:11:54 +0600

    Solved unknown person wrongly prediction

--- Face Recognition with Attendance/main.py
+++ Face Recognition with Attendance/main.py
@@ -1,3 +1,4 @@
+
 import cv2
 
 import numpy as np
@@ -63,20 +64,19 @@ while True:
             for encodeface , faceLoc in zip(encode_Currentframe,face_Currentframe):
                 matches = face_recognition.compare_faces(knownImg_encodeList, encodeface)
                 faceDis = face_recognition.face_distance(knownImg_encodeList, encodeface)
-                    
-                    # print(faceDis)
-                    
-                matchIndex = np.argmin(faceDis)
-                if matches[matchIndex]:
-                    name = classNames[matchIndex].upper()
-                    # print(name)
-                    y1,x2,y2,x1=faceLoc
-                    y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
-                    cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
-                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                    markAttendance(name)
-        
+                
+                if min(faceDis) < 0.45:                     
+                    matchIndex = np.argmin(faceDis)
+                    if matches[matchIndex]:
+                        name = classNames[matchIndex].upper()
+                        
+                        y1,x2,y2,x1=faceLoc
+                        y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                        cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+                        cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+                        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                        markAttendance(name)
+            
             cv2.imshow('Webcam', img)
             key = cv2.waitKey(1)
             if key == ord("q"):
